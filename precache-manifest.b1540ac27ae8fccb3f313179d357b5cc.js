47f342e9839f14233ed68cfef4ecf84883c5c321
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2019-07-30 23:05:45 -0400

    new build, added header and many design/animation elements

---
+++ precache-manifest.b1540ac27ae8fccb3f313179d357b5cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0a8fb29001c915a7dd523f4cebd369",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49033b1264291b3e0758",
+    "url": "/static/css/main.0ee517bd.chunk.css"
+  },
+  {
+    "revision": "1dc329414e0679beb04c",
+    "url": "/static/js/2.19a19115.chunk.js"
+  },
+  {
+    "revision": "49033b1264291b3e0758",
+    "url": "/static/js/main.8069f715.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
