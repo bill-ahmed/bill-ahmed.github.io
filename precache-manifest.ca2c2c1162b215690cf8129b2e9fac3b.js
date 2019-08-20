b04bc1989ef4e6773bda502e46f1170a267bfd81
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2019-08-20 17:03:14 -0400

    new build - added data to gallery section

---
+++ precache-manifest.ca2c2c1162b215690cf8129b2e9fac3b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d3fc0fa661b4a64434da209b0ead62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e7129acf4e8fca730d8",
+    "url": "/static/css/main.8297b8d0.chunk.css"
+  },
+  {
+    "revision": "677e1c76d09bc8a1f9db",
+    "url": "/static/js/2.1dfcdcd7.chunk.js"
+  },
+  {
+    "revision": "8e7129acf4e8fca730d8",
+    "url": "/static/js/main.5ce07289.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "40cba9e0f44868b42f0c9a31bfdc4fae",
+    "url": "/static/media/ddsblaze_homepage.40cba9e0.jpg"
+  },
+  {
+    "revision": "756553f2f6324db83270a082dd7e910b",
+    "url": "/static/media/ddsblaze_send_bulk_message.756553f2.jpg"
+  },
+  {
+    "revision": "1f4f948d8312135cb70bf9065ae11e75",
+    "url": "/static/media/ddsblaze_undo_checkin.1f4f948d.jpg"
+  },
+  {
+    "revision": "62d8b6ef8e6c796a5ddad822dff96874",
+    "url": "/static/media/facebook-brands.62d8b6ef.svg"
+  },
+  {
+    "revision": "045f997c1221322246cdd8be0580b226",
+    "url": "/static/media/fire-solid.045f997c.svg"
+  },
+  {
+    "revision": "ec5bc1b3148ca464a3a3e17af89c3ead",
+    "url": "/static/media/github-brands.ec5bc1b3.svg"
+  },
+  {
+    "revision": "6a8f9dd8112d82d4ec2e0ffaa6f09c6b",
+    "url": "/static/media/laptop-code-solid.6a8f9dd8.svg"
+  },
+  {
+    "revision": "1e01f0c0dea217ba568ebbcec49c5b6b",
+    "url": "/static/media/linkedin-in-brands.1e01f0c0.svg"
+  },
+  {
+    "revision": "f70f1a06a42d4b1769e29a3fcf70cb92",
+    "url": "/static/media/microchip-solid.f70f1a06.svg"
+  },
+  {
+    "revision": "59eb4593c9244f5390ff838d2decf9a5",
+    "url": "/static/media/money-check-alt-solid.59eb4593.svg"
+  },
+  {
+    "revision": "785ad8c44b7e2f2e11bc7556a8f5a3cc",
+    "url": "/static/media/motion_pong_gameplay_1_noAudio.785ad8c4.mp4"
+  },
+  {
+    "revision": "b80343c42079e74c07a815595ce6c3fb",
+    "url": "/static/media/motion_pong_splashscreen.b80343c4.jpg"
+  },
+  {
+    "revision": "27a62134c0b74b04a21ce8e6ac9ec13d",
+    "url": "/static/media/ultrasonic_sensor_1.27a62134.jpg"
+  },
+  {
+    "revision": "4c197f5d01d10c1ffbdea41400ccce0e",
+    "url": "/static/media/ultrasonic_sensor_2.4c197f5d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d8294373114a0ecc972159dfbe85f12.js"
+  "/precache-manifest.ca2c2c1162b215690cf8129b2e9fac3b.js"
 );
 
 self.addEventListener('message', (event) => {
